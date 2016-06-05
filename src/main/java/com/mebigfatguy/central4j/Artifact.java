9993d5432e534258e94b49a8e8bc97945d3807c5
Dave Brosius <dbrosius@mebigfatguy.com>
2016-06-04 21:56:22 -0400

    stub in basic files

---
+++ src/main/java/com/mebigfatguy/central4j/Artifact.java
@@ -0,0 +1,103 @@
+/*
+ * central4j - an api for accessing maven central
+ * Copyright 2016 MeBigFatGuy.com
+ * Copyright 2016 Dave Brosius
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.mebigfatguy.central4j;
+
+public class Artifact implements Comparable<Artifact> {
+
+    private String groupId;
+    private String artifactId;
+    private String type;
+    private String version;
+    private String classifier;
+
+    public Artifact(String groupId, String artifactId, String type, String classifier, String version) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.type = type;
+        this.classifier = classifier;
+        this.version = version;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getClassifier() {
+        return classifier;
+    }
+
+    @Override
+    public int hashCode() {
+        return groupId.hashCode() ^ artifactId.hashCode() ^ type.hashCode() ^ classifier.hashCode() ^ version.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Artifact)) {
+            return false;
+        }
+
+        Artifact that = (Artifact) o;
+        return groupId.equals(that.groupId) && artifactId.equals(that.artifactId) && type.equals(that.type) && classifier.equals(that.classifier)
+                && version.equals(that.version);
+    }
+
+    @Override
+    public int compareTo(Artifact a) {
+        int cmp = groupId.compareTo(a.groupId);
+        if (cmp != 0) {
+            return cmp;
+        }
+
+        cmp = artifactId.compareTo(a.artifactId);
+        if (cmp != 0) {
+            return cmp;
+        }
+
+        cmp = type.compareTo(a.type);
+        if (cmp != 0) {
+            return cmp;
+        }
+
+        cmp = classifier.compareTo(a.classifier);
+        if (cmp != 0) {
+            return cmp;
+        }
+        ;
+
+        return version.compareTo(a.version);
+    }
+
+    @Override
+    public String toString() {
+        return "Artifact [groupId=" + groupId + ", artifactId=" + artifactId + ", type=" + type + (!classifier.isEmpty() ? ", classifier=" + classifier : "")
+                + ", version=" + version + "]";
+    }
+}
