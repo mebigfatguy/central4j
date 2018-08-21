891ffd03c2726896fbb27acf6d530198ae8b526f
Dave Brosius <dbrosius@mebigfatguy.com>
2018-08-20 22:22:40 -0400

    fix test

--- src/test/java/com/mebigfatguy/central4j/CentralRepositoryTest.java
+++ src/test/java/com/mebigfatguy/central4j/CentralRepositoryTest.java
@@ -96,7 +96,7 @@ public class CentralRepositoryTest {
 
         String version = r.getLatestVersion("com.mebigfatguy.yank", "yank");
 
-        Assert.assertEquals("2.0.0", version);
+        Assert.assertEquals("2.0.1", version);
     }
 
     @Test
